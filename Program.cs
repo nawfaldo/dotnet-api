ad297b48b764e79df64427937ab37225ddb4ffa0
nawfaldo <nawfaldo.main@gmail.com>
2024-07-18 15:04:20 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using test.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
